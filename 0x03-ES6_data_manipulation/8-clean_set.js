63c4ca2554b6206b3a5647bf265d2026fe896674
yomex96 <abayomirobertonawole2@gmail.com>
2024-07-02 06:32:51 -0700

    bovi

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,25 @@
-export default function cleanSet(set, string) {
-    if (!string || !string.length) return '';
-    let value = '';
-    for (const el of set) {
-      if (el && el.startsWith(string)) {
-        value
-          += value.length === 0 ? el.replace(string, '') : el.replace(string, '-');
+/**
+ * Joins a set of strings with a dash after stripping the strings of
+ * a leading sub string.
+ * @param {Set<String>} set - A collection of strings.
+ * @param {String} startString - The string to strip from the beginning
+ * of each item in the set.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @returns {String}
+ */
+export default function cleanSet(set, startString) {
+  const parts = [];
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
+
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
       }
     }
-    return value;
   }
+  return parts.join('-');
+}
